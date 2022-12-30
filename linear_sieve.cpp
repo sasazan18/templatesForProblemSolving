bc29892be45ea737925e79fd470425e88f5b9335
Saad Ahmed Sazan <63748593+sazan007@users.noreply.github.com>
2022-12-30 07:38:53 +0600

    Add files via upload
    committed!

---
+++ linear_sieve.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mxn 1000005
+int lp[mxn+1]; //least prime
+vector<int>prime;
+
+
+void linear_sieve(){
+    for (int i = 2; i <= mxn; i++)
+    {
+        if(lp[i]==0){
+            lp[i]=i;
+            prime.push_back(i);
+        }
+        for(int j=0;j<prime.size() && i*prime[j]<=mxn && prime[j]<=lp[i]  ; ++j ){
+            lp[i*prime[j]]=prime[j];
+            //we cut each number only once!
+            //we will store the least prime for each number.
+            //prime[j]<=lp[i] for the case where we might store a prime for a number which is not least!
+            //ex : i=6, prime--2,3,5 
+            //we might do this-- 12=2,18=3,30=5 but for 18 and 30 it should be 2.
+        }
+    }
+    
+}
+
+
+
+
+int main(){
+    linear_sieve();
+}
